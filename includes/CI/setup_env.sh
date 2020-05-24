375f7e7ffef821a3098fbf116fa432c5294ab74b
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-24 20:55:39 +0200

    basic zfs tryout

--- includes/CI/setup_env.sh
+++ includes/CI/setup_env.sh
@@ -1,2 +1,7 @@
 sudo pkg install -y py37-iocage
+truncate -s 20g tmpdisk.img
+sudo f. -h
+sudo mdconfig -a -t vnode -f tmpdisk.img -u 0)
+sudo zpool create -f -o ashift=12 testpool /dev/md0
+sudo zfs get mountpoint testpool
 echo "this is env setup"
