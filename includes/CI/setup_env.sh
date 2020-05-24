c1113a4d3748b6ee82ee768f7900b028fe09bca3
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-24 21:08:33 +0200

    damnit its late

--- includes/CI/setup_env.sh
+++ includes/CI/setup_env.sh
@@ -1,6 +1,6 @@
 sudo pkg install -y py37-iocage
 truncate -s 20g tmpdisk.img
-sudo f. -h
+sudo df. -h
 sudo mdconfig -a -t vnode -f tmpdisk.img -u 0
 sudo zpool create -f -o ashift=12 testpool /dev/md0
 sudo zfs get mountpoint testpool
