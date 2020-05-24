1f6d21b850730a9b3cda3fca1ddb9d8b57d0b551
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-24 22:13:38 +0200

    this should work

--- includes/CI/setup_env.sh
+++ includes/CI/setup_env.sh
@@ -1,10 +1,9 @@
 sudo pkg install -y py37-iocage
+sudo mkdir /data
 echo "starting truncate"
-truncate -s 50g tmpdisk.img
-echo "starting mdconfig"
-sudo mdconfig -a -t vnode -f tmpdisk.img -u 0
+truncate -s 50g /data/tmpdisk.img
 echo "starting zpool"
-sudo zpool create -f testpool /dev/md0
+sudo zpool create testpool /data/tmpdisk.img
 echo "starting zfs"
 sudo zfs get mountpoint testpool
 echo "this is env setup"
