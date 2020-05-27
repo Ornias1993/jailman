e77ee0d1a0328fbcc377c33f61e32e924c529b40
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-27 22:03:07 +0200

    Fix oopsy

--- blueprints/bazarr/update.sh
+++ blueprints/bazarr/update.sh
@@ -7,7 +7,7 @@ initblueprint "$1"
 # Initialise defaults
 
 iocage exec "$1" service bazarr stop
-iocage exec "$1" service bazarr stop git -C /usr/local/share/bazarr pull
+iocage exec "$1" git -C /usr/local/share/bazarr pull
 iocage exec "$1" "pip install -r /usr/local/share/bazarr/requirements.txt"
 iocage exec "$1" chown -R bazarr:bazarr /usr/local/share/bazarr /config
 cp "${SCRIPT_DIR}"/blueprints/bazarr/includes/bazarr.rc /mnt/"${global_dataset_iocage}"/jails/"$1"/root/usr/local/etc/rc.d/bazarr
